498608259364bb0e33752543421b8366dd67b2d4
Dev.Pantiss <dev.pantiss@DevPantisss-MacBook-Air.local>
2024-10-01 12:09:15 +0530

    login form update

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import style from "@/components/common/input/input.module.css";
 
 const Login: React.FC = () => {
   const router = useRouter();
@@ -50,38 +51,25 @@ const Login: React.FC = () => {
           <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
             Log In
           </h2>
-          <form className="space-y-6" onSubmit={handleLogin}>
-            <div>
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Username or email
-              </label>
+          <form className="space-y-10" onSubmit={handleLogin}>
+            <div className={`${style.brutalist_container} mt-12`}>
               <input
+                placeholder="Enter Your Email"
+                className={`${style.brutalist_input} ${style.smooth_type}`}
                 type="text"
-                id="username"
-                style={{ padding: "12px 16px" }}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                required
               />
+              <label className={`${style.brutalist_label}`}>EMAIL</label>
             </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
+
+            <div className={`${style.brutalist_container} mt-12`}>
               <input
+                placeholder="Enter Your Password"
+                className={`${style.brutalist_input} ${style.smooth_type}`}
                 type="password"
-                id="password"
-                style={{ padding: "12px 16px" }}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                required
               />
+              <label className={`${style.brutalist_label}`}>PASSWORD</label>
             </div>
-            <div>
+            <div className="mt-8">
               <button
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
