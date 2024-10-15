a74e88143e7dbec9ae8a1da68a9304add1040fc9
adi5689 <adityakumarsahu.89@gmail.com>
2024-10-15 16:31:34 +0530

    upload certificate field change

--- src/pages/lms/certification.tsx
+++ src/pages/lms/certification.tsx
@@ -341,32 +341,6 @@ const Certification: React.FC = () => {
                   )}
                 </div>
               </div>
-
-              {/* Reference Videos */}
-              {/* <div className="flex flex-col gap-8">
-                {referenceVideos.map((video, index) => (
-                  <div
-                    key={index}
-                    className="bg-gray-700 rounded-lg overflow-hidden shadow-lg"
-                  >
-                    <iframe
-                      width="100%"
-                      height="200"
-                      src={video.url}
-                      title={video.title}
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowFullScreen
-                      className="w-full h-full"
-                    ></iframe>
-                    <div className="p-4">
-                      <h3 className="text-xl font-semibold text-white">
-                        {video.title}
-                      </h3>
-                    </div>
-                  </div>
-                ))}
-              </div> */}
             </div>
           ) : (
             // No View: Options to Upload Certificate or Join Academy
@@ -400,13 +374,13 @@ const Certification: React.FC = () => {
 
               {/* Upload Certificate View */}
               {selectedOption === "upload" && (
-                <div className="flex flex-col lg:flex-row items-center bg-gray-800 p-6 rounded-lg shadow-lg">
+                <div className="flex flex-col-reverse lg:flex-row items-center bg-gray-800 p-6 rounded-lg shadow-lg">
                   {/* Left Section: Organization Selection */}
                   <div className="lg:w-1/2 w-full pr-4">
                     <h2 className="text-2xl text-white font-semibold mb-4">
                       Select Organization
                     </h2>
-                    <div className="grid grid-cols-3 gap-4">
+                    <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
                       {organizations.map((org) => (
                         <div
                           key={org.name}
@@ -441,17 +415,65 @@ const Certification: React.FC = () => {
 
                   {/* Right Section: Certificate Upload */}
                   {selectedOrg && (
-                    <div className="lg:w-1/2 w-full pl-4">
+                    <div className="lg:w-1/2 w-full lg:pl-4 mb-6">
                       <h2 className="text-2xl text-white font-semibold mb-4">
                         Upload your certificate for {selectedOrg}
                       </h2>
+
+                      {/* Custom File Upload Field */}
                       {!certificatePreview ? (
-                        <input
-                          type="file"
-                          accept="image/*"
-                          onChange={handleFileChange}
-                          className="w-full p-2 text-white bg-black border-white rounded-lg"
-                        />
+                        <label
+                          htmlFor="certificateUpload"
+                          className="labelFile flex flex-col justify-center w-full h-[190px] items-center p-5 rounded-md border-dotted border-2 text-white border-white cursor-pointer"
+                        >
+                          <span>
+                            <svg
+                              viewBox="0 0 184.69 184.69"
+                              width="60px"
+                              height="60px"
+                            >
+                              <g>
+                                <g>
+                                  <g>
+                                    <path
+                                      d="M149.968,50.186c-8.017-14.308-23.796-22.515-40.717-19.813
+                                     C102.609,16.43,88.713,7.576,73.087,7.576c-22.117,0-40.112,17.994-40.112,40.115c0,0.913,0.036,1.854,0.118,2.834
+                                     C14.004,54.875,0,72.11,0,91.959c0,23.456,19.082,42.535,42.538,42.535h33.623v-7.025H42.538
+                                     c-19.583,0-35.509-15.929-35.509-35.509c0-17.526,13.084-32.621,30.442-35.105c0.931-0.132,1.768-0.633,2.326-1.392
+                                     c0.555-0.755,0.795-1.704,0.644-2.63c-0.297-1.904-0.447-3.582-0.447-5.139c0-18.249,14.852-33.094,33.094-33.094
+                                     c13.703,0,25.789,8.26,30.803,21.04c0.63,1.621,2.351,2.534,4.058,2.14c15.425-3.568,29.919,3.883,36.604,17.168
+                                     c0.508,1.027,1.503,1.736,2.641,1.897c17.368,2.473,30.481,17.569,30.481,35.112c0,19.58-15.937,35.509-35.52,35.509H97.391
+                                     v7.025h44.761c23.459,0,42.538-19.079,42.538-42.535C184.69,71.545,169.884,53.901,149.968,50.186z"
+                                      style={{ fill: "white" }}
+                                    ></path>
+                                  </g>
+                                  <g>
+                                    <path
+                                      d="M108.586,90.201c1.406-1.403,1.406-3.672,0-5.075L88.541,65.078
+                                     c-0.701-0.698-1.614-1.045-2.534-1.045l-0.064,0.011c-0.018,0-0.036-0.011-0.054-0.011c-0.931,0-1.85,0.361-2.534,1.045
+                                     L63.31,85.127c-1.403,1.403-1.403,3.672,0,5.075c1.403,1.406,3.672,1.406,5.075,0L82.296,76.29v97.227
+                                     c0,1.99,1.603,3.597,3.593,3.597c1.979,0,3.59-1.607,3.59-3.597V76.165l14.033,14.036
+                                     C104.91,91.608,107.183,91.608,108.586,90.201z"
+                                      style={{ fill: "white" }}
+                                    ></path>
+                                  </g>
+                                </g>
+                              </g>
+                            </svg>
+                          </span>
+                          <p className="text-center">
+                            drag and drop your file here or click to select a
+                            file!
+                          </p>
+                          <input
+                            className="input hidden"
+                            type="file"
+                            name="certificateUpload"
+                            id="certificateUpload"
+                            onChange={handleFileChange}
+                            accept="image/*"
+                          />
+                        </label>
                       ) : (
                         <div className="relative flex justify-center items-center">
                           <img
